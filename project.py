3531f882659acddeba9b1ebfa4c525d18d03af54
Virgil Anderson <Virgil.Anderson@gmail.com>
2018-12-04 15:29:54 -0700

    feat: Added routes to project.py
    The 8 basic routes have been added to project.py
    These include routes for: show catalog, new category,
    delete category, show category, create new item,
    edit an item and delete an item.

--- project.py
+++ project.py
@@ -4,8 +4,44 @@ app = Flask(__name__)
 # Routes
 # Show catalog
 @app.route('/')
+@app.route('/catalog')
 def showCatalog():
     return "This page will show all my categories"
+
+# Create a new category
+@app.route('/category/new')
+def newCategory():
+    return "This page will enable you to add a new item to the category"
+
+# Edit a category
+@app.route('/category/category_id/edit')
+def editCategory():
+    return "This page will allow me to edit a category"
+
+# Delete a category
+@app.route('/category/category_id/delete')
+def deleteCategory():
+    return "This page will enable category deletion"
+
+# Show a category
+@app.route('/category/category_id')
+def showCategory():
+    return "This page will show all the items in a particular category"
+
+# Create a new item
+@app.route('/item/category_id/new')
+def newItem():
+    return "This page will enable new item creation"
+
+# Edit an item
+@app.route('/item/category_id/item_id/edit')
+def editItem():
+    return "This page will enable item edits"
+
+# Delete an item
+@app.route('/item/category_id/item_id/delete')
+def deleteItem():
+    return "This page will enable item deletion"
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
