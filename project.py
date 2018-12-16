19551e1c38251484cccda5b874937ed93293888e
Virgil Anderson <Virgil.Anderson@gmail.com>
2018-12-16 14:07:01 -0700

    Bug: Added oauth2callback

--- project.py
+++ project.py
@@ -93,7 +93,7 @@ def gconnect():
     stored_access_token = login_session.get('access_token')
     stored_gplus_id = login_session.get('gplus_id')
     if stored_access_token is not None and gplus_id == stored_gplus_id:
-        response = make_response(json.dumps('User already connected.'), 200)
+        response = make_response(json.dumps('Current user is already connected.'),200)
         response.headers['Content-Type'] = 'application/json'
         return response
 
