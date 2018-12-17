af84dbfc94b56336ced43b706c257b9b9d3f0339
Virgil Anderson <Virgil.Anderson@gmail.com>
2018-12-17 14:24:44 -0700

    Refactor: Pep 8 style

--- project.py
+++ project.py
@@ -4,8 +4,13 @@
 # By Virgil Anderson
 # December 13, 2018
 
-from flask import Flask, render_template, request
-from flask import redirect, url_for, flash, jsonify
+from flask import (Flask,
+                   render_template,
+                   request,
+                   redirect,
+                   url_for,
+                   flash,
+                   jsonify)
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, magicCategory, Item, User
